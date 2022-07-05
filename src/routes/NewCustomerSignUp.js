23a86891622a45b9611fa5c1890417a366bc0afb
jjteitel <98434018+jjteitel@users.noreply.github.com>
2022-07-05 16:34:15 -0400

    update customer signup page

--- src/routes/NewCustomerSignUp.js
+++ src/routes/NewCustomerSignUp.js
@@ -49,6 +49,27 @@ export default function SignUpCustomer() {
                     </Typography>
                     <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
                         <Grid container spacing={2}>
+                            <Grid item xs={12} sm={6}>
+                                <TextField
+                                    autoComplete="given-name"
+                                    name="firstName"
+                                    required
+                                    fullWidth
+                                    id="firstName"
+                                    label="First Name"
+                                    autoFocus
+                                />
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    id="lastName"
+                                    label="Last Name"
+                                    name="lastName"
+                                    autoComplete="family-name"
+                                />
+                            </Grid>
                             <Grid item xs={12}>
                                 <TextField
                                     required
