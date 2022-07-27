ee42b2a629d4ec72fc591e5e245b3cc5f7ce7f8f
jjteitel <98434018+jjteitel@users.noreply.github.com>
2022-07-26 23:49:45 -0400

    add styling for customer profile page

--- src/routes/CustomerProfile.js
+++ src/routes/CustomerProfile.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import {useEffect, useState} from "react";
 import axios from "axios";
+import {Button, Divider, Image} from "semantic-ui-react";
+import {Link} from "react-router-dom";
+// import "./CustomerProfile.css"
 
 function CustomerProfile() {
 
     const [customer, setCustomer] = useState(null);
     const email = localStorage.getItem('userId');
+    const src = 'https://www.pulsecarshalton.co.uk/wp-content/uploads/2016/08/jk-placeholder-image.jpg'
 
     useEffect(() => {
         axios.post('https://e4zbw0wbnk.execute-api.us-east-1.amazonaws.com/test/get', jsonData)
@@ -33,9 +37,42 @@ function CustomerProfile() {
 
     return (
         <div>
-            <p>Info</p>
-            <p>Name: {customer.Item.firstName.S} {customer.Item.lastName.S}</p>
-            <p>Email: {customer.Item.email.S}</p>
+            <section className="Welcome-Section" style={
+                {
+                    display: 'flex',
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    backgroundPosition: "center",
+                    backgroundRepeat: "no-repeat",
+                    backgroundSize: "cover",
+                    backgroundImage: "linear-gradient(to right, #43e97b 0%, #38f9d7 100%)",
+                    fontWeight: "bold",
+                    height: "20vh"
+                }
+            }>
+                <h1 style={{fontFamily: "Beba Neue", fontSize: "7vh"}}>
+                    Welcome {customer.Item.firstName.S} {customer.Item.lastName.S}
+                </h1>
+                <div>
+                    <Button size='small' inverted>
+                        <Link to="/businesses" style={{color: 'black'}}> Check Out Participating Businesses </Link>
+                    </Button>
+                    <Button size='small inverted'>
+                        <Link to="/AboutUs" style={{color: 'black'}}> Get to Know Us </Link>
+                    </Button>
+                </div>
+            </section>
+            <section>
+                <div style={{marginBottom: '2em', marginTop: '2em'}}>
+                    <h1>Your Wallet</h1>
+                    <h3>No Badges Yet</h3>
+                    <Image.Group size='medium'>
+                        <Image src={src} />
+                        <Image src={src} />
+                        <Image src={src} />
+                    </Image.Group>
+                </div>
+            </section>
         </div>
     )
 }
